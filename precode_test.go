c9ce778bb423c4c5a97cac52f95c86735f2981a1
FolderWithFolders <maga.saidoff2014@yandex.ru>
2025-01-13 22:44:51 +0300

    mini fixes and comments

--- precode_test.go
+++ precode_test.go
@@ -50,37 +50,37 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 }
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
+	totalCount := 4                                                     // Устанавливаем ожидаемое количество кафе
+	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil) // Создаем запрос с параметрами count=5 и city=moscow
 
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := httptest.NewRecorder() // Создаем запись ответа
+	handler := http.HandlerFunc(mainHandle)    // Создаем обработчик
+	handler.ServeHTTP(responseRecorder, req)   // Отправляем запрос обработчику
 
-	require.Equal(t, http.StatusOK, responseRecorder.Code)
-	assert.NotEmpty(t, responseRecorder.Body.String())
-	assert.Len(t, strings.Split(responseRecorder.Body.String(), ","), totalCount)
+	require.Equal(t, http.StatusOK, responseRecorder.Code)                        // Проверяем, что код ответа равен 200
+	assert.NotEmpty(t, responseRecorder.Body.String())                            // Проверяем, что тело ответа не пустое
+	assert.Len(t, strings.Split(responseRecorder.Body.String(), ","), totalCount) // Проверяем, что количество кафе в ответе равно ожидаемому
 }
 
 func TestMainHandlerWhenCityIsWrong(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=1&city=wrongcity", nil)
+	req := httptest.NewRequest("GET", "/cafe?count=1&city=wrongcity", nil) // Создаем запрос с параметрами count=1 и city=wrongcity
 
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := httptest.NewRecorder() // Создаем запись ответа
+	handler := http.HandlerFunc(mainHandle)    // Создаем обработчик
+	handler.ServeHTTP(responseRecorder, req)   // Отправляем запрос обработчику
 
-	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
-	assert.Equal(t, "wrong city value", responseRecorder.Body.String())
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)      // Проверяем, что код ответа равен 400
+	assert.Equal(t, "wrong city value", responseRecorder.Body.String()) // Проверяем, что тело ответа содержит сообщение "wrong city value"
 }
 
 func TestMainHandlerWhenRequestIsCorrect(t *testing.T) {
-	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil) // Создаем запрос с параметрами count=2 и city=moscow
 
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
+	responseRecorder := httptest.NewRecorder() // Создаем запись ответа
+	handler := http.HandlerFunc(mainHandle)    // Создаем обработчик
+	handler.ServeHTTP(responseRecorder, req)   // Отправляем запрос обработчику
 
-	require.Equal(t, http.StatusOK, responseRecorder.Code)
-	assert.NotEmpty(t, responseRecorder.Body.String())
-	assert.Len(t, strings.Split(responseRecorder.Body.String(), ","), 2)
+	require.Equal(t, http.StatusOK, responseRecorder.Code)               // Проверяем, что код ответа равен 200
+	assert.NotEmpty(t, responseRecorder.Body.String())                   // Проверяем, что тело ответа не пустое
+	assert.Len(t, strings.Split(responseRecorder.Body.String(), ","), 2) // Проверяем, что количество кафе в ответе равно 2
 }
